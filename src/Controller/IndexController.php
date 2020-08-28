07ddcd8cc3e07d34e500225bfa92ffd79aaccdd7
unknown <stagiaire@B-8-BWDJT02.b-doranco.prod>
2020-08-28 16:53:32 +0200

    premier commit pour instruire la partie backend du BBshop

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+// le controller va retourner quelque chose dans un fichier twig pour l'afficher. Les templates dans le meme temps ont été 
+// crée... Il faut ensuite aller dans index.html.twig qui m'a été créé par la même occasion
+//Ce que l'on va voir en dessous ===> il s'agit d'une toute qui  va diriger,rediriger, vers une URL
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/index", name="index") 
+     */
+
+     
+    public function index()
+    {
+        
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+}
